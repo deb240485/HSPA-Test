7340b7c94fe7ccf60d85702e2680014020976c9a
deb240485 <deb240485rana@gmail.com>
2023-01-26 17:21:59 +0530

    Added Multiple properties

--- src/app/services/housing.service.ts
+++ src/app/services/housing.service.ts
@@ -14,12 +14,23 @@ export class HousingService {
   constructor(private http:HttpClient) { }
 
   getAllProperties(SellRent: number): Observable<IPropertyBase[]>{
+
     return this.http.get('data/properties.json').pipe(
       map(data=>{
-        const propertiesArray: Array<IPropertyBase> =[];
+        const propertiesArray: Array<IPropertyBase> = [];
+        const localProperties = JSON.parse(localStorage.getItem('newProp')!);
+
+        if (localProperties) {
+          for (const id in localProperties) {
+            if (localProperties.hasOwnProperty(id) && localProperties[id].SellRent === SellRent) {
+              propertiesArray.push(localProperties[id]);
+            }
+          }
+        }
+
         for (const id in data)
         {
-          if(data.hasOwnProperty(id) && data[id].SellRent === SellRent)
+          if (data.hasOwnProperty(id) && data[id].SellRent === SellRent)
           {
             propertiesArray.push(data[id]);
           }
@@ -32,7 +43,28 @@ export class HousingService {
   }
 
   addProperty(property: Property){
-    localStorage.setItem('newProp', JSON.stringify(property));
+
+    //localStorage.setItem('newProp', JSON.stringify(property));
+    let newProp = [property];
+
+
+    // Add new property in array if newProp already exists in local storage.
+    if (localStorage.getItem('newProp')){
+      newProp = [property,
+                  ...JSON.parse(localStorage.getItem('newProp')!)];
+    }
+
+    localStorage.setItem('newProp', JSON.stringify(newProp));
+  }
+
+  newPropID(){
+    if (localStorage.getItem('PID')){
+      localStorage.setItem('PID', String(+localStorage.getItem('PID')! + 1));
+      return +localStorage.getItem('PID')!;
+    }else{
+      localStorage.setItem('PID', '101');
+      return 101;
+    }
   }
 
 }
